303c2bbb3ba9725266244c0d957d471fc6b7e91d
zhiboXun <93247880+zhiboXun@users.noreply.github.com>
2021-11-25 15:41:57 +0800

    test commit

--- src/main/java/com/tegao/test/controller/TestController.java
+++ src/main/java/com/tegao/test/controller/TestController.java
@@ -1,10 +1,20 @@
 package com.tegao.test.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author xunzhibo
  * @date 2021/11/25 11:30
  */
-
+@Log4j2
+@RestController
+@RequestMapping("/test")
 public class TestController {
-
+    @RequestMapping("/data")
+    public String testData(){
+        log.info("测试数据");
+        return "hello word";
+    }
 }
