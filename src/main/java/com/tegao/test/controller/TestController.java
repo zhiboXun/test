feaa0cb7fc8505253c9b4e1868632de5a6b5f695
zhiboXun <93247880+zhiboXun@users.noreply.github.com>
2021-11-25 13:32:15 +0800

    first commit

---
+++ src/main/java/com/tegao/test/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.tegao.test.controller;
+
+/**
+ * @author xunzhibo
+ * @date 2021/11/25 11:30
+ */
+
+public class TestController {
+
+}
---
+++ src/test/java/com/tegao/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tegao.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
